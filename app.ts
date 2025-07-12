38e077fb6aa351ebcc735c5814f6ec25f41ec7c0
JessieKimhj <dpro5893@gmail.com>
2025-07-12 16:04:03 -0700

    second commit

--- app.ts
+++ app.ts
@@ -1,13 +1,26 @@
 
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
+
 
 const app = new Hono();
 
 app.use("*", logger())
 app.use("*", cors())
+
+app.use("*", serveStatic({root: "./frontend/dist"}))
+app.get("/*", async (c) => {
+    try{
+        const indexHTML = await Bun.file("./frontend/dist/index.html").text()
+        return c.html(indexHTML)
+    }
+    catch(error){
+        console.log(error)
+    }
+})
 
 export default app;
 
@@ -17,5 +30,5 @@ const server = serve({
     port: PORT, 
     fetch:app.fetch,
 })
-
+console.log("Hi")
 console.log("server running on port: ", PORT)
