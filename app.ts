7e023bc0e9a4d96548cae84c08d60fe0cbfdaa54
JessieKimhj <dpro5893@gmail.com>
2025-07-12 15:25:10 -0700

    initial commit

---
+++ app.ts
@@ -0,0 +1,21 @@
+
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use("*", logger())
+app.use("*", cors())
+
+export default app;
+
+const PORT = parseInt(process.env.PORT!) || 3333;
+
+const server = serve({
+    port: PORT, 
+    fetch:app.fetch,
+})
+
+console.log("server running on port: ", PORT)
